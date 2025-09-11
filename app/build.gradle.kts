044d9fad22afa48abfe8d65ed76c948511458786
Gabriel Loureiro <157625824+GabrielLoureiro09@users.noreply.github.com>
2025-09-11 11:53:25 -0300

    feat/splash-screen: subindo a primeira versao da splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
