8d93d32cf10e848da4dc1717f8f87882aebc4f37
Gabriel Loureiro <157625824+GabrielLoureiro09@users.noreply.github.com>
2025-09-18 08:19:54 -0300

    feat/tela-cadastro: Fazendo a tela de cadastro

--- app/src/main/java/com/example/igestamobile/Cadastro.java
+++ app/src/main/java/com/example/igestamobile/Cadastro.java
@@ -2,8 +2,7 @@ package com.example.igestamobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.widget.ImageView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bumptech.glide.Glide;
-
-public class SplashScreen extends AppCompatActivity {
+public class Cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ImageView imageView = findViewById(R.id.logo);
-        Glide.with(this).load(R.mipmap.logoanimadasemfundo).into(imageView);
-        new Handler().postDelayed(this::abrirTela, 7500);
-    }
-    private void abrirTela(){
-        Intent rota = new Intent(this, Cadastro.class);
-        startActivity(rota);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-        finish();
+        Button btn_cadastrar = findViewById(R.id.btn_cadastrar);
+        btn_cadastrar.setOnClickListener(v -> {
+            Intent rota = new Intent(this, MainActivity.class);
+            startActivity(rota);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            finish();
+        });
     }
 }
