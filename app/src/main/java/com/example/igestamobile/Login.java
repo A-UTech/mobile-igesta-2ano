c887e9e594d266428da103c2f00aad98c2f85731
Gabriel Loureiro <157625824+GabrielLoureiro09@users.noreply.github.com>
2025-09-25 10:24:23 -0300

    feat/tela-cadastro: Subindo o front da tela de cadastro

--- app/src/main/java/com/example/igestamobile/Login.java
+++ app/src/main/java/com/example/igestamobile/Login.java
@@ -1,8 +1,6 @@
 package com.example.igestamobile;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Cadastro extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        Button btn_cadastrar = findViewById(R.id.cadastrar_bt);
-        btn_cadastrar.setOnClickListener(v -> {
-            Intent rota = new Intent(this, MainActivity.class);
-            startActivity(rota);
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-            finish();
         });
     }
 }
