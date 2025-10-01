23957b8813bd27783eebee5d969401298f30a692
Gabriel Loureiro <157625824+GabrielLoureiro09@users.noreply.github.com>
2025-10-01 08:18:42 -0300

    feat/backend-selecionar-condenas: Fazer backend da tela de selecionar condenas, conectado com a api

--- app/src/main/java/com/example/igestamobile/ui/SelecionarCondenas.java
+++ app/src/main/java/com/example/igestamobile/ui/SelecionarCondenas.java
@@ -1,6 +1,9 @@
 package com.example.igestamobile.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -27,11 +30,11 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SelecionarCondenas extends AppCompatActivity {
-    private final List<CondenaModel> condenaList = new ArrayList<>();
     private RecyclerView recyclerView;
     private CondenaAdapter adapter;
     private CondenaApi condenaApi;
 
+    private Button btSelecionar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +49,15 @@ public class SelecionarCondenas extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         fetchCondenas();
+
+        btSelecionar = findViewById(R.id.bt_selecionar);
+
+        btSelecionar.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+            List<CondenaModel> condenasSelecionadas = adapter.getCondenasSelecionadas();
+        });
     }
 
     private void setupRetrofit() {
